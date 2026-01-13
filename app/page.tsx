a8125aeb4aa72f7382719ff1291623e432bb78ce
srinivas k <kolipakasrinivas456@gmail.com>
2026-01-13 06:56:47 +0530

    using client fetching for apis

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,68 @@
-export const dynamic = "force-dynamic";
-
+"use client";
 import ProductsGrid from "./components/ProductsGrid";
 import { Product } from "@/app/types";
+import { useState, useEffect } from "react";
+// async function getProducts(): Promise<Product[]> {
+//   try {
+//     const res = await fetch("https://fakestoreapi.com/products", {
+//       cache: "no-store",
+//     });
 
-async function getProducts(): Promise<Product[]> {
-  try {
-    const res = await fetch("https://fakestoreapi.com/products", {
-      cache: "no-store",
-    });
+//     if (!res.ok) {
+//       console.error("[getProducts] API error:", res.status);
+//       return [];
+//     }
 
-    if (!res.ok) {
-      console.error("[getProducts] API error:", res.status);
-      return [];
+//     return await res.json();
+//   } catch (err) {
+//     console.error("[getProducts] Fetch failed:", err);
+//     return [];
+//   }
+// }
+
+import { ProductCardSkeleton } from "@/app/loading/ProductCardSkeleton";
+export default function Home() {
+  // const products = await getProducts();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products", {
+          cache: "no-store",
+        });
+
+        if (!res.ok) {
+          console.error("[getProducts] API error:", res.status);
+          return;
+        }
+
+        const data: Product[] = await res.json();
+        setProducts(data);
+      } catch (err) {
+        console.error("[getProducts] Fetch failed:", err);
+      } finally {
+        setLoading(false);
+      }
     }
 
-    return await res.json();
-  } catch (err) {
-    console.error("[getProducts] Fetch failed:", err);
-    return [];
-  }
-}
+    getProducts();
+  }, []);
 
-export default async function Home() {
-  const products = await getProducts();
+  if (loading) {
+    return (
+      <div className="mt-20">
+        <p className="text-center mt-10 text-gray-500">
+          No products available right now. Please check back later.
+        </p>
+        <div className="grid grid-cols-1 place-items-center md:place-items-stretch md:grid-cols-2 lg:grid-cols-3 gap-5 px-2 pt-4 md:px-18">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <ProductCardSkeleton key={i} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <main>
